dda55ef58822e05d2adc19bd8543f7bf43bf9c17
AmanRawatOfficial <amanrawat4060@gmail.com>
2022-03-16 11:20:01 +0530

    Created needed components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+/* Local Components */
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+
+import "./App.scss";
+
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="app">
+            <Router>
+                <Header></Header>
+                <Route path="/" component={Home} />
+                <Route path="/movie/:imdbID" component={MovieDetail} />
+                <Route component={PageNotFound} />
+                <Footer />
+            </Router>
+        </div>
+    )
 }
 export default App;
